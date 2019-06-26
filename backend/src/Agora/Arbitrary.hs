387f0c2515254e553bee0203458e1c428088b221
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2019-06-26 09:44:29 +0300

    [AG-2] Proper mocking, including pagination
    Problem: Mock handlers do pretty much nothing and yield constant (and empty)
    results regardless of query parameters, which is not quite convenient for
    testing API on a frontend side.
    
    Solution: deterministically generate mock data and implement proper selection
    of this data as well as pagination.

--- backend/src/Agora/Arbitrary.hs
+++ backend/src/Agora/Arbitrary.hs
@@ -3,6 +3,8 @@
 -}
 module Agora.Arbitrary
        ( arbitraryByteString
+       , arbitrarySentence
+       , detGen
        ) where
 
 import qualified Data.ByteString as BS
@@ -10,9 +12,10 @@ import Data.ByteString.Base58 (bitcoinAlphabet, encodeBase58)
 import qualified Data.Text as T
 import Data.Time.Calendar (Day, fromGregorian)
 import Data.Time.Clock (DiffTime, UTCTime (..), secondsToDiffTime)
-import Test.QuickCheck (Arbitrary (..), Gen, arbitraryBoundedEnum, choose, elements, vector,
-                        vectorOf)
+import Test.QuickCheck (Arbitrary (..), arbitraryBoundedEnum, choose, elements, vector, vectorOf)
 import Test.QuickCheck.Arbitrary.Generic (genericArbitrary)
+import Test.QuickCheck.Gen (Gen (..))
+import Test.QuickCheck.Random
 
 import Agora.Types
 
@@ -27,6 +30,11 @@ randomWords = ["patak", "bardaq", "skovoroda"]
 -- | Helper for generating a random sentence with a given length.
 arbitrarySentence :: Int -> Gen Text
 arbitrarySentence n = T.unwords <$> vectorOf n (elements randomWords)
+
+-- | Runs a generator (@Gen@ value) with a given integer seed
+-- and size 30 (which is the same as @generate@ uses)
+detGen :: Int -> Gen a -> a
+detGen seed gen = unGen gen (mkQCGen seed) 30
 
 instance Arbitrary DiffTime where
   arbitrary = secondsToDiffTime <$> choose (0, 86400)
--- backend/src/Agora/Web/Handlers.hs
+++ backend/src/Agora/Web/Handlers.hs
@@ -5,9 +5,16 @@ module Agora.Web.Handlers
        ( agoraHandlers
        ) where
 
+import Control.Monad.Except (throwError)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
 import Servant.API.Generic (ToServant)
+import Servant.Server (Handler, ServantErr (..), err404)
 import Servant.Server.Generic (AsServer, genericServer)
+import Servant.Util.Dummy (paginate)
+import Test.QuickCheck (arbitrary, vector, vectorOf)
 
+import Agora.Arbitrary
 import Agora.Types
 import Agora.Web.API
 
@@ -16,26 +23,34 @@ type AgoraHandlers = ToServant AgoraEndpoints AsServer
 -- | Server handler implementation for Agora API.
 agoraHandlers :: AgoraHandlers
 agoraHandlers = genericServer AgoraEndpoints
-  { aePeriod = \_periodNum -> pure examplePeriod
-  , aeProposals = \_periodNum _pagination -> pure []
-  , aeProposalVotes = \_periodNum _pagination -> pure []
-  , aeBallots = \_periodNum _pagination -> pure []
+  { aePeriod = \mPeriodNum ->
+      view _1 <$> getPeriod (fromMaybe 20 mPeriodNum)
+
+  , aeProposals = \periodNum pagination ->
+      paginate pagination . view _2 <$> getPeriod periodNum
+
+  , aeProposalVotes = \periodNum pagination ->
+      paginate pagination . view _3 <$> getPeriod periodNum
+
+  , aeBallots = \periodNum pagination ->
+      paginate pagination . view _4 <$> getPeriod periodNum
   }
 
--- | Mock data for `GET /period`.
-examplePeriod :: PeriodInfo
-examplePeriod = PeriodInfo
-  { _piPeriod = Period
-    { _pNum = 0
-    , _pType = Proposing
-    , _pStartLevel = 0
-    , _pCycle = 4
-    }
-  , _piVoteStats = Just $ VoteStats 1000 4000
-  , _piProposal = Just Proposal
-    { _pHash = Hash "asdfasdfasdfas"
-    , _pTitle = "Pataq"
-    , _pDescription = "bardaq"
-    }
-  , _piBallots = Just $ Ballots 30 40 5
-  }
+-- | All possible data about the period (for the mock)
+type PeriodData = (PeriodInfo, [Proposal], [ProposalVote], [Ballot])
+
+-- | Gets all period data, if a period exists, or fails with 404.
+getPeriod :: Word -> Handler PeriodData
+getPeriod n = M.lookup n mockApiData `whenNothing` throwError noSuchPeriod
+  where noSuchPeriod = err404
+          { errBody = "Period with given number does not exist" }
+
+-- | Mock data for API, randomly generated with a particular seed.
+mockApiData :: Map Word PeriodData
+mockApiData = M.fromList $ zipWith setNum [1..] periods
+  where setNum i pd = (i, pd & _1.piPeriod.pNum .~ i)
+        periods = detGen 42 $ vectorOf 20 $ (,,,)
+          <$> arbitrary
+          <*> vector 1000
+          <*> vector 1000
+          <*> vector 1000
