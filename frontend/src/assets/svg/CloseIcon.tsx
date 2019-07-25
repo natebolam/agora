0201ede89440a275c2a72ea0e544cd8f35d759fc
Damir Garifullin <damir.garifullin@serokell.io>
2019-07-25 04:37:54 +0300

    [AG-17] Added mobile header menu.
    Problem: No mobile menu with links to tezos materials.
    
    Solution: Added mobile menu. Fixed background colors to match design.

---
+++ frontend/src/assets/svg/CloseIcon.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent, ReactElement } from "react";
+
+const CloseIcon: FunctionComponent = (): ReactElement => (
+  <svg width={27} height={26}>
+    <path
+      d="M2 1l24 24M2 25L26 1"
+      fill="none"
+      fillRule="evenodd"
+      stroke="#2C7DF7"
+      strokeLinejoin="round"
+      strokeWidth={3}
+    />
+  </svg>
+);
+
+export default CloseIcon;
--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,20 +1,61 @@
-import React, { FunctionComponent, ReactElement } from "react";
+import React, { FunctionComponent, ReactElement, useState } from "react";
+import cx from "classnames";
 import Logo from "~/assets/png/logo.png";
 import styles from "~/styles/components/common/Header.scss";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import MenuIcon from "~/assets/svg/MenuIcon";
+import CloseIcon from "~/assets/svg/CloseIcon";
+
+interface HeaderMenuTypes {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const HeaderMenu: FunctionComponent<HeaderMenuTypes> = ({
+  isOpen,
+  onClose,
+}): ReactElement => {
+  const body = document.getElementsByTagName("body").item(0);
+  if (body) {
+    body.setAttribute(
+      "style",
+      isOpen ? "position: fixed" : "position: initial"
+    );
+  }
+  const { t } = useTranslation();
+  return (
+    <div
+      className={cx(styles.header__menu, {
+        [styles.header__menu_open]: isOpen,
+      })}
+    >
+      <div className={styles.header__menu__closeButton} onClick={onClose}>
+        <CloseIcon />
+      </div>
+      <div className={styles.header__logo}>
+        <Link to="/">
+          <img alt="" src={Logo} />
+          {t("header.logoCaption")}
+        </Link>
+      </div>
+      <div className={styles.header__menu__links}>
+        <a href="#">{t("header.wikiLink")}</a>
+        <a href="#">{t("header.getStartedLink")}</a>
+        <a href="#">{t("header.governanceLink")}</a>
+      </div>
+    </div>
+  );
+};
 
 const AgoraHeader: FunctionComponent = (): ReactElement => {
   const { t } = useTranslation();
-
+  const [isMenuOpen, setMenuOpen] = useState(false);
   return (
     <div className={styles.header}>
       <div className={styles.header__logo}>
         <Link to="/">
           <img alt="" src={Logo} />
-        </Link>
-        <Link to="/" className={styles.header__logo__img}>
           {t("header.logoCaption")}
         </Link>
       </div>
@@ -23,8 +64,17 @@ const AgoraHeader: FunctionComponent = (): ReactElement => {
         <a href="#">{t("header.getStartedLink")}</a>
         <a href="#">{t("header.governanceLink")}</a>
       </div>
-      <div className={styles.header__expandMenuButton}>
+      <div
+        className={styles.header__expandMenuButton}
+        onClick={(): void => setMenuOpen(true)}
+      >
         <MenuIcon />
+      </div>
+      <div>
+        <HeaderMenu
+          isOpen={isMenuOpen}
+          onClose={(): void => setMenuOpen(false)}
+        />
       </div>
     </div>
   );
