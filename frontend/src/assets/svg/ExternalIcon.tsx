a2bf0f3e1ace34890717d16b9a7e84426c7e3cf8
Damir Garifullin <damir.garifullin@serokell.io>
2019-08-02 12:35:33 +0300

    [AG-68] Replaced icon in discourse button w/ svg.
    Problem: Discourse button uses png icon, so it looks bad on HiDPI screens.
    
    Solution: Replaced w/ svg.

---
+++ frontend/src/assets/svg/ExternalIcon.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from "react";
+
+const ExternalIcon = (): ReactElement => (
+  <svg width={12} height={12}>
+    <path
+      fill="#FFF"
+      d="M8.586 2H5V0h7v7h-2V3.414L4.707 8.707 3.293 7.293 8.586 2zM11 10v2H0V1h2v9h9z"
+    />
+  </svg>
+);
+
+export default ExternalIcon;
