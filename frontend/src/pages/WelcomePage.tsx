01a69ada5b823fcb89a1cd515ccbe5f5b9e40372
Leonid Cupikov <dreambooms@gmail.com>
2019-08-28 12:18:08 +0300

    [AG-99] Add status of the ongoing vote on welcome page

--- frontend/src/pages/WelcomePage.tsx
+++ frontend/src/pages/WelcomePage.tsx
@@ -5,9 +5,20 @@ import { ButtonLink } from "~/components/common/ButtonLink";
 import styles from "~/styles/pages/WelcomePage.scss";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
-import { PeriodType } from "~/models/Period";
+import {
+  PeriodType,
+  BallotsStats,
+  ExplorationPeriodInfo,
+  ProposalPeriodInfo,
+  VoteStats,
+} from "~/models/Period";
 import { RootStoreType } from "~/store";
 import Logo from "~/assets/svg/Logo";
+import TimesIcon from "~/assets/svg/TimesIcon";
+import CheckIcon from "~/assets/svg/CheckIcon";
+import SvgUpIcon from "~/assets/svg/UpIcon";
+import { Proposal } from "~/models/ProposalInfo";
+import { ProposalsListItem } from "~/models/ProposalsList";
 
 const WelcomePageHeader: FunctionComponent = (): ReactElement => {
   const { t } = useTranslation();
@@ -68,6 +79,10 @@ interface CurrentPeriodInfoTypes {
   periodType?: PeriodType;
   timeRemaining?: string;
   curLevel?: number;
+  ballots?: BallotsStats;
+  voteStats?: VoteStats;
+  proposals?: ProposalsListItem[];
+  winner?: Proposal;
 }
 
 const CurrentPeriodInfo: FunctionComponent<CurrentPeriodInfoTypes> = ({
@@ -77,6 +92,10 @@ const CurrentPeriodInfo: FunctionComponent<CurrentPeriodInfoTypes> = ({
   periodType,
   timeRemaining,
   curLevel = 0,
+  ballots,
+  voteStats,
+  proposals,
+  winner,
 }): ReactElement => {
   const { t } = useTranslation();
 
@@ -84,11 +103,41 @@ const CurrentPeriodInfo: FunctionComponent<CurrentPeriodInfoTypes> = ({
   const fraction = ((curLevel + 1) % 4096) / 4096;
   const width = 100 - (Math.floor(fraction * 4) / 4) * 100 + "%";
 
+  const getStatus = (): JSX.Element | string => {
+    if (periodType == "testing") return "";
+    if (periodType == "proposal") {
+      if (!winner || !proposals) return "";
+      const sum = proposals.reduce((a, b): number => a + b.votesCasted, 0);
+      const percentage = Math.round((winner.votesCasted / sum) * 10000) / 100;
+      return (
+        <>
+          {`${winner.title}: ${percentage}%`} <SvgUpIcon />
+        </>
+      );
+    }
+
+    if (!ballots || !voteStats) return "";
+
+    const percentage =
+      Math.round((ballots.yay / (ballots.yay + ballots.nay)) * 10000) / 100;
+    const isQuorum =
+      (voteStats.votesCast / voteStats.votesAvailable) * 100 >= ballots.quorum;
+
+    return (
+      <>
+        {`${percentage}%`} <SvgUpIcon />
+        {" | Quorum "}
+        {isQuorum ? <CheckIcon /> : <TimesIcon />}
+      </>
+    );
+  };
+
   return (
     <div className={cx(className, styles.welcomePage__period)}>
       <div className={styles.welcomePage__period__header}>
         {t("welcome.currentPeriod.header")}
       </div>
+      <div className={styles.welcomePage__period__status}>{getStatus()}</div>
       <ButtonLink
         className={styles.welcomePage__period__button}
         href={`/period/${currentPeriodId}`}
@@ -128,6 +177,10 @@ const WelcomePage: FunctionComponent = (): ReactElement => {
     currentCycle?: number;
     discourseLink?: string;
     curLevel?: number;
+    ballots?: BallotsStats;
+    voteStats?: VoteStats;
+    proposals?: ProposalsListItem[];
+    winner?: Proposal;
   }
 
   const periodInfo: PeriodInfoTypes = useSelector(
@@ -152,6 +205,16 @@ const WelcomePage: FunctionComponent = (): ReactElement => {
         curLevel: state.periodStore.period
           ? state.periodStore.period.period.curLevel
           : undefined,
+        ballots: state.periodStore.period
+          ? (state.periodStore.period as ExplorationPeriodInfo).ballots
+          : undefined,
+        voteStats: state.periodStore.period
+          ? (state.periodStore.period as ExplorationPeriodInfo).voteStats
+          : undefined,
+        proposals: state.periodStore.proposals || undefined,
+        winner: state.periodStore.period
+          ? (state.periodStore.period as ProposalPeriodInfo).winner
+          : undefined,
       };
     }
   );
@@ -168,6 +231,10 @@ const WelcomePage: FunctionComponent = (): ReactElement => {
             timeRemaining={periodInfo.endTime}
             currentCycle={periodInfo.currentCycle}
             curLevel={periodInfo.curLevel}
+            ballots={periodInfo.ballots}
+            voteStats={periodInfo.voteStats}
+            proposals={periodInfo.proposals}
+            winner={periodInfo.winner}
           />
         </div>
       </div>
