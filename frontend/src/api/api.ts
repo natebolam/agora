39024b0d4481de6bbf3c565a9ba8fd50c144e629
Aleksandr Pakulev <sashasashasasha151@gmail.com>
2019-12-25 16:59:26 +0300

    [SDAO-18] Remove baseURL changes

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import { AgoraApi } from "./AgoraApi";
 
 const axiosConfig: AxiosRequestConfig = {
-  baseURL: "http://localhost:8190/api/v1",
+  baseURL: "/api/v1",
 };
 
 const axiosIntance: AxiosInstance = axios.create(axiosConfig);
