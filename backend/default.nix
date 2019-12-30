96bc794ad389a46365bc39e5742ad9417cba705f
Alexander Bantyev <balsoft@balsoft.ru>
2019-12-30 13:32:00 +0300

    Switch to haskell.nix

--- backend/default.nix
+++ backend/default.nix
@@ -1,45 +1,32 @@
-{ pkgs ? import ./../nix {}, shell ? false }: with pkgs;
+{ _expose ? false }:
+
 let
-  withPostgreSQL = drv: haskell.lib.overrideCabal drv (a: {
-    testToolDepends = (a.testToolDepends or []) ++
-      [ ephemeralpg postgresql getopt ];
-    preCheck = (a.preCheck or "") + ''
-      export TEST_PG_CONN_STRING=$(pg_tmp -w 600)
-    '';
-    # we need the temporary directory from pg_tmp
-    # so extract it out of $TEST_PG_CONN_STRING
-    postCheck = (a.postCheck or "") + ''
-      pg_tmp stop -d $(echo ''${TEST_PG_CONN_STRING#*=} | sed 's:%2F:/:g') || :
-    '';
-  });
+  pkgs = import ../nix { };
+  inherit (pkgs) haskell;
 
-  ignoreFilter = path: type:
-    let
-      inherit (lib) removePrefix hasPrefix hasSuffix;
-      relPath = removePrefix (toString ./. + "/") (toString path);
-      baseName = baseNameOf relPath;
-    in
-      !(
-        baseName == ".gitignore" ||
-        hasSuffix ".md" baseName ||
-        hasSuffix ".nix" baseName
-      );
-
-  # Set a constant name for the src closure
-  source = let
-    root = ./.;
-  in builtins.path {
-    name = "agora-backend-src";
-    path = root;
-    filter = name: type:
-      (ignoreFilter name type && gitignoreFilter root name type && lib.cleanSourceFilter name type);
+  project = pkgs.haskell-nix.stackProject {
+    src = pkgs.haskell-nix.haskellLib.cleanGit {
+      src = ../.;
+      subDir = "backend";
+    };
+    modules = [
+      ({ pkgs, ... }: {
+        packages = {
+          agora = {
+            preCheck = ''
+              export TEST_PG_CONN_STRING=$(pg_tmp -w 600)
+            '';
+            # we need the temporary directory from pg_tmp
+            # so extract it out of $TEST_PG_CONN_STRING
+            postCheck = ''
+              pg_tmp stop -d $(echo ''${TEST_PG_CONN_STRING#*=} | sed 's:%2F:/:g') || :
+            '';
+          };
+          components.tests.agora-test = {
+            depends = with pkgs; [ ephemeralpg getopt postgresql ];
+          };
+        };
+      })
+    ];
   };
-in
-
-stackToNix {
-  root = source;
-  inherit shell;
-  overrides = _: super: {
-    agora = (withPostgreSQL super.agora);
-  };
-}
+in project.agora
