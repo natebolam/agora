bc8baf9786130595f8cd1840ab1955323c1bfe93
Alexander Bantyev <balsoft@balsoft.ru>
2019-12-30 16:23:58 +0300

    fixup! Switch to haskell.nix

--- backend/default.nix
+++ backend/default.nix
@@ -12,19 +12,8 @@ let
     modules = [
       ({ pkgs, ... }: {
         packages = {
-          agora = {
-            preCheck = ''
-              export TEST_PG_CONN_STRING=$(pg_tmp -w 600)
-            '';
-            # we need the temporary directory from pg_tmp
-            # so extract it out of $TEST_PG_CONN_STRING
-            postCheck = ''
-              pg_tmp stop -d $(echo ''${TEST_PG_CONN_STRING#*=} | sed 's:%2F:/:g') || :
-            '';
-          };
-          components.tests.agora-test = {
-            depends = with pkgs; [ ephemeralpg getopt postgresql ];
-          };
+          loot-log.components.library.doHaddock = false;
+          agora.components.all.dontCheck = true;
         };
       })
     ];
