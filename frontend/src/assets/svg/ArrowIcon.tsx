82276efef7f8865f2212570b576a8680f3fbc4d6
Aleksandr Pakulev <sashasashasasha151@gmail.com>
2019-12-31 13:28:44 +0500

    [SDAO-19] Reskin agora

--- frontend/src/assets/svg/ArrowIcon.tsx
+++ frontend/src/assets/svg/ArrowIcon.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from "react";
 
 const SvgArrow = (): ReactElement => (
   <svg width={18} height={18}>
-    <g fill="none" fillRule="evenodd" stroke="#FFF" strokeWidth={2}>
+    <g fill="none" fillRule="evenodd" stroke="#006298" strokeWidth={2}>
       <path strokeLinejoin="round" d="M2 9h16" />
       <path d="M10 17L2 9l8-8" />
     </g>
