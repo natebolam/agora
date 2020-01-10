786acad96330fe51719a3d143abf1848fb449764
Aleksandr Pakulev <sashasashasasha151@gmail.com>
2020-01-10 16:47:29 +0300

    [SDAO-44] Add error page

--- frontend/src/application.tsx
+++ frontend/src/application.tsx
@@ -3,13 +3,16 @@ import "~/styles/main.scss";
 import { withRedux } from "~/store";
 import { Router, View } from "react-navi";
 import agoraRouter from "~/router/agoraRouter";
+import ErrorPage from "./pages/ErrorPage";
 
 function Application(): ReactElement {
   return (
     <Router routes={agoraRouter()}>
-      <Suspense fallback={null}>
-        <View />
-      </Suspense>
+      <ErrorPage>
+        <Suspense fallback={null}>
+          <View />
+        </Suspense>
+      </ErrorPage>
     </Router>
   );
 }
--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,56 +1,59 @@
 import React from "react";
 import { FunctionComponent, ReactElement } from "react";
 import styles from "~/styles/pages/ErrorPage.scss";
-import { Link } from "react-router-dom";
+import { Link } from "react-navi";
 import Helmet from "react-helmet";
 import { useTranslation } from "react-i18next";
-import useRouter from "use-react-router";
 import Logo from "~/assets/svg/Logo";
+import { NotFoundBoundary } from "react-navi";
 
-const ErrorPage: FunctionComponent = (): ReactElement => {
+const ErrorPage: FunctionComponent = ({ children }): ReactElement => {
   const { t } = useTranslation();
-  const { match } = useRouter();
-  const errorCodeMatch: RegExpMatchArray | null = match.path.match(
-    /\/error\/(\d+)/
-  );
-  const errorCode: string = errorCodeMatch ? errorCodeMatch[1] : "404";
+  const errorCode: string = "404";
 
   return (
-    <div className={styles.errorPage__wrapper}>
-      <Helmet bodyAttributes={{ style: "background-color : #fff" }} />
+    <NotFoundBoundary
+      render={() => (
+        <div className={styles.errorPage__wrapper}>
+          <Helmet bodyAttributes={{ style: "background-color : #fff" }} />
 
-      <div className={styles.errorPage}>
-        <div className={styles.errorPage__logo}>
-          <Link to="/" className={styles.errorPage__logo__content}>
-            <Logo />
-            <div>{t("header.logoCaption")}</div>
-          </Link>
-        </div>
-        <main className={styles.errorPage__content}>
-          <div>
-            <div className={styles.errorPage__content__caption}>
-              {t("errorPage.errorCodeCaption")}
+          <div className={styles.errorPage}>
+            <div className={styles.errorPage__logo}>
+              <Link href="/" className={styles.errorPage__logo__content}>
+                <Logo />
+              </Link>
             </div>
-            <div className={styles.errorPage__content__code}>{errorCode}</div>
+            <main className={styles.errorPage__content}>
+              <div>
+                <div className={styles.errorPage__content__caption}>
+                  {t("errorPage.errorCodeCaption")}
+                </div>
+                <div className={styles.errorPage__content__code}>
+                  {errorCode}
+                </div>
+              </div>
+            </main>
+            <footer className={styles.errorPage__footer}>
+              <div className={styles.errorPage__footer__content}>
+                {errorCode === "404" ? (
+                  <>
+                    <p className={styles.header}>
+                      {t(`errorPage.errors.${errorCode}.errorCaption`)}
+                    </p>
+                    <p className={styles.description}>
+                      {t(`errorPage.errors.${errorCode}.errorDescription`)}
+                    </p>
+                  </>
+                ) : null}
+                <Link href="/stage">{t("errorPage.homeButtonCaption")}</Link>
+              </div>
+            </footer>
           </div>
-        </main>
-        <footer className={styles.errorPage__footer}>
-          <div className={styles.errorPage__footer__content}>
-            {errorCode === "404" || errorCode === "500" ? (
-              <>
-                <p className={styles.header}>
-                  {t(`errorPage.errors.${errorCode}.errorCaption`)}
-                </p>
-                <p className={styles.description}>
-                  {t(`errorPage.errors.${errorCode}.errorDescription`)}
-                </p>
-              </>
-            ) : null}
-            <Link to="/">{t("errorPage.homeButtonCaption")}</Link>
-          </div>
-        </footer>
-      </div>
-    </div>
+        </div>
+      )}
+    >
+      {children}
+    </NotFoundBoundary>
   );
 };
 
